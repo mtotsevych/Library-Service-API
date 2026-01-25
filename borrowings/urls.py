c80afcb0db88db4b71b273bc88fbdbd0dfb6ec58
Mykola Totsevych <totsevich02@gmail.com>
2026-01-25 19:40:48 +0100

    add return endpoint route for borrowing

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from borrowings.views import (
     BorrowingDetailView,
     BorrowingListCreateView,
+    BorrowingReturnView,
 )
 
 app_name = "borrowings"
@@ -10,4 +11,9 @@ app_name = "borrowings"
 urlpatterns = [
     path("", BorrowingListCreateView.as_view(), name="borrowing-list-create"),
     path("<int:pk>/", BorrowingDetailView.as_view(), name="borrowing-detail"),
+    path(
+        "<int:pk>/return/",
+        BorrowingReturnView.as_view(),
+        name="borrowing-return"
+    ),
 ]

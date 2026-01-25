0cf16646664a2d06c9ebf856224218199e580752
Mykola Totsevych <totsevich02@gmail.com>
2026-01-25 15:34:47 +0100

    add str method and default ordering to Book model

--- books/models.py
+++ books/models.py
@@ -11,3 +11,9 @@ class Book(models.Model):
     cover = models.CharField(max_length=30, choices=CoverChoices.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ("inventory", "title",)

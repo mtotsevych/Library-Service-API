c7866b6c4b425858f8f68c8a09a69adf2421da88
Mykola Totsevych <totsevich02@gmail.com>
2026-01-25 15:22:58 +0100

    add user registration view using CreateAPIView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = ()
+    permission_classes = (AllowAny,)

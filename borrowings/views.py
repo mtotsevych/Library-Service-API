b85777a9573aba3a85593afde94b89e714d67f24
Mykola Totsevych <totsevich02@gmail.com>
2026-01-25 19:11:29 +0100

    add filtering to list view and apply isborrower permission to detail view

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,8 +1,11 @@
+from django.db.models import QuerySet
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.serializers import Serializer
 
 from books.models import Book
 from borrowings.models import Borrowing
+from borrowings.permissions import IsBorrower
 from borrowings.serializers import (
     BorrowingListSerializer,
     BorrowingDetailSerializer,
@@ -11,7 +14,7 @@ from borrowings.serializers import (
 
 
 class BorrowingListCreateView(generics.ListCreateAPIView):
-    queryset = Borrowing.objects.select_related("user", "book")
+    permission_classes = (IsAuthenticated,)
 
     @staticmethod
     def borrow_one_book(book: Book) -> None:
@@ -27,7 +30,24 @@ class BorrowingListCreateView(generics.ListCreateAPIView):
         borrowing = serializer.save(user=self.request.user)
         self.borrow_one_book(borrowing.book)
 
+    def get_queryset(self) -> QuerySet[Borrowing]:
+        queryset = Borrowing.objects.select_related("user", "book")
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        if is_active == "1":
+            queryset = queryset.filter(actual_return_date=None)
+        if user_id and self.request.user.is_staff:
+            queryset = queryset.filter(user_id=int(user_id))
+
+        return queryset
+
 
 class BorrowingDetailView(generics.RetrieveAPIView):
     queryset = Borrowing.objects.select_related("user", "book")
     serializer_class = BorrowingDetailSerializer
+    permission_classes = (IsBorrower,)

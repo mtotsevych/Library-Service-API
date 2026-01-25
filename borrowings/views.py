3c69a4c67b72ad712cdfc28e7459672443357332
Mykola Totsevych <totsevich02@gmail.com>
2026-01-25 17:31:51 +0100

    add list and detail API views with optimized queryset

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingListSerializer,
+    BorrowingDetailSerializer
+)
+
+
+class BorrowingListView(generics.ListAPIView):
+    queryset = Borrowing.objects.select_related("user", "book")
+    serializer_class = BorrowingListSerializer
+
+
+class BorrowingDetailView(generics.RetrieveAPIView):
+    queryset = Borrowing.objects.select_related("user", "book")
+    serializer_class = BorrowingDetailSerializer

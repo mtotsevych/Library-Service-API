1200b25edc2cbf2f65c899d108fd23687a5170d8
Mykola Totsevych <totsevich02@gmail.com>
2026-01-25 15:26:40 +0100

    add authenticated user management view (retrieve/update)

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from users.models import User
 from users.serializers import UserSerializer
 
 
@@ -8,3 +9,11 @@ class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
     authentication_classes = ()
     permission_classes = (AllowAny,)
+
+
+class UserManageView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self) -> User:
+        return self.request.user

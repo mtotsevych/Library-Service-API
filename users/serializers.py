116cc745909cc8af5f2a76b7f05d6b927d4587b9
Mykola Totsevych <totsevich02@gmail.com>
2026-01-25 15:22:10 +0100

    add serializer for creating and updating User model

---
+++ users/serializers.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+        )
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "style": {"input_type": "password"}
+            }
+        }
+
+    def create(self, validated_data: dict) -> User:
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        """Update a user, set the password correctly and return it"""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user

4942f2fa9be1c9d0795ebf19f2873c8801da351e
Mykola Totsevych <totsevich02@gmail.com>
2026-01-25 15:24:14 +0100

    add registration route to users URL configuration

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from users.views import UserCreateView
+
 urlpatterns = [
+    path("", UserCreateView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]

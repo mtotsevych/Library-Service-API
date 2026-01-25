6b02c13aa38f356b783c28f1d3b6a1a610fa4b26
Mykola Totsevych <totsevich02@gmail.com>
2026-01-25 15:27:15 +0100

    add /me route for authenticated user management

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from users.views import UserCreateView
+from users.views import UserCreateView, UserManageView
 
 urlpatterns = [
     path("", UserCreateView.as_view(), name="register"),
+    path("me", UserManageView.as_view(), name="me"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
